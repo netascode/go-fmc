b49bf28e93f8024c9ca3aa358a200f6dbbd086a6
danischm <danischm@cisco.com>
2024-01-06 17:11:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+[![Tests](https://github.com/netascode/go-fmc/actions/workflows/test.yml/badge.svg)](https://github.com/netascode/go-fmc/actions/workflows/test.yml)
+
+# go-fmc
+
+`go-fmc` is a Go client library for Cisco Secure FMC (Firewall Management Center). It is based on Nathan's excellent [goaci](https://github.com/brightpuddle/goaci) module and features a simple, extensible API and [advanced JSON manipulation](#result-manipulation).
+
+## Getting Started
+
+### Installing
+
+To start using `go-fmc`, install Go and `go get`:
+
+`$ go get -u github.com/netascode/go-fmc`
+
+### Basic Usage
+
+```go
+package main
+
+import "github.com/netascode/go-fmc"
+
+func main() {
+    client, _ := fmc.NewClient("https://1.1.1.1", "user", "pwd")
+
+    res, _ := client.Get("/api/fmc_config/v1/domain/{DOMAIN_UUID}/object/networks")
+    println(res.Get("items.0.name").String())
+}
+```
+
+This will print something like:
+
+```
+any-ipv4
+```
+
+#### Result manipulation
+
+`fmc.Result` uses GJSON to simplify handling JSON results. See the [GJSON](https://github.com/tidwall/gjson) documentation for more detail.
+
+```go
+res, _ := client.Get("/api/fmc_config/v1/domain/{DOMAIN_UUID}/object/networks")
+
+for _, obj := range res.Get("items").Array() {
+    println(obj.Get("@pretty").String()) // pretty print network objects
+}
+```
+
+#### POST data creation
+
+`fmc.Body` is a wrapper for [SJSON](https://github.com/tidwall/sjson). SJSON supports a path syntax simplifying JSON creation.
+
+```go
+body := fmc.Body{}.
+    Set("name", "net1").
+    Set("value", "1.5.4.0/24")
+client.Post("/api/fmc_config/v1/domain/{DOMAIN_UUID}/object/networks", body.Str)
+```
+
+## Documentation
+
+See the [documentation](https://godoc.org/github.com/netascode/go-fmc) for more details.
---
+++ client.go
@@ -0,0 +1,389 @@
+// Package fmc is a Cisco Secure FMC (Firewall Management Center) REST client library for Go.
+package fmc
+
+import (
+	"bytes"
+	"crypto/tls"
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"net/http/cookiejar"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/tidwall/gjson"
+
+	"github.com/juju/ratelimit"
+)
+
+const DefaultMaxRetries int = 3
+const DefaultBackoffMinDelay int = 2
+const DefaultBackoffMaxDelay int = 60
+const DefaultBackoffDelayFactor float64 = 3
+
+// Client is an HTTP FMC client.
+// Use fmc.NewClient to initiate a client.
+// This will ensure proper cookie handling and processing of modifiers.
+type Client struct {
+	// HttpClient is the *http.Client used for API requests.
+	HttpClient *http.Client
+	// Url is the FMC IP or hostname, e.g. https://10.0.0.1:443 (port is optional).
+	Url string
+	// Authentication token is the current authentication token
+	AuthToken string
+	// Refresh token is the current authentication token
+	RefreshToken string
+	// Usr is the FMC username.
+	Usr string
+	// Pwd is the FMC password.
+	Pwd string
+	// Insecure determines if insecure https connections are allowed.
+	Insecure bool
+	// Maximum number of retries
+	MaxRetries int
+	// Minimum delay between two retries
+	BackoffMinDelay int
+	// Maximum delay between two retries
+	BackoffMaxDelay int
+	// Backoff delay factor
+	BackoffDelayFactor float64
+	// Authentication mutex
+	AuthenticationMutex *sync.Mutex
+	// LastRefresh is the timestamp of the last authentication token refresh
+	LastRefresh time.Time
+	// RefreshCount is the number to authentication token refreshes with the same refresh token
+	RefreshCount int
+	// DomainUUID is the UUID of the domain returned when generating a token
+	DomainUUID string
+
+	RateLimiterBucket *ratelimit.Bucket
+}
+
+// NewClient creates a new FMC HTTP client.
+// Pass modifiers in to modify the behavior of the client, e.g.
+//
+//	client, _ := NewClient("fmc1.cisco.com", "user", "password", RequestTimeout(120))
+func NewClient(url, usr, pwd string, mods ...func(*Client)) (Client, error) {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	cookieJar, _ := cookiejar.New(nil)
+	httpClient := http.Client{
+		Timeout:   60 * time.Second,
+		Transport: tr,
+		Jar:       cookieJar,
+	}
+
+	client := Client{
+		HttpClient:          &httpClient,
+		Url:                 url,
+		Usr:                 usr,
+		Pwd:                 pwd,
+		MaxRetries:          DefaultMaxRetries,
+		BackoffMinDelay:     DefaultBackoffMinDelay,
+		BackoffMaxDelay:     DefaultBackoffMaxDelay,
+		BackoffDelayFactor:  DefaultBackoffDelayFactor,
+		AuthenticationMutex: &sync.Mutex{},
+		RateLimiterBucket:   ratelimit.NewBucketWithQuantum(time.Minute, 100, 100),
+	}
+
+	for _, mod := range mods {
+		mod(&client)
+	}
+	return client, nil
+}
+
+// Insecure determines if insecure https connections are allowed. Default value is true.
+func Insecure(x bool) func(*Client) {
+	return func(client *Client) {
+		client.HttpClient.Transport.(*http.Transport).TLSClientConfig.InsecureSkipVerify = x
+	}
+}
+
+// RequestTimeout modifies the HTTP request timeout from the default of 60 seconds.
+func RequestTimeout(x time.Duration) func(*Client) {
+	return func(client *Client) {
+		client.HttpClient.Timeout = x * time.Second
+	}
+}
+
+// MaxRetries modifies the maximum number of retries from the default of 3.
+func MaxRetries(x int) func(*Client) {
+	return func(client *Client) {
+		client.MaxRetries = x
+	}
+}
+
+// BackoffMinDelay modifies the minimum delay between two retries from the default of 2.
+func BackoffMinDelay(x int) func(*Client) {
+	return func(client *Client) {
+		client.BackoffMinDelay = x
+	}
+}
+
+// BackoffMaxDelay modifies the maximum delay between two retries from the default of 60.
+func BackoffMaxDelay(x int) func(*Client) {
+	return func(client *Client) {
+		client.BackoffMaxDelay = x
+	}
+}
+
+// BackoffDelayFactor modifies the backoff delay factor from the default of 3.
+func BackoffDelayFactor(x float64) func(*Client) {
+	return func(client *Client) {
+		client.BackoffDelayFactor = x
+	}
+}
+
+// NewReq creates a new Req request for this client.
+// Use a "{DOMAIN_UUID}" placeholder in the URI to be replaced with the domain UUID.
+func (client Client) NewReq(method, uri string, body io.Reader, mods ...func(*Req)) Req {
+	u := strings.ReplaceAll(uri, "{DOMAIN_UUID}", client.DomainUUID)
+	httpReq, _ := http.NewRequest(method, client.Url+u, body)
+	req := Req{
+		HttpReq:    httpReq,
+		LogPayload: true,
+	}
+	for _, mod := range mods {
+		mod(&req)
+	}
+	return req
+}
+
+// Do makes a request.
+// Requests for Do are built ouside of the client, e.g.
+//
+//	req := client.NewReq("GET", "/api/fmc_config/v1/domain/{DOMAIN_UUID}/object/networks", nil)
+//	res, _ := client.Do(req)
+func (client *Client) Do(req Req) (Res, error) {
+	// add token
+	req.HttpReq.Header.Add("X-auth-access-token", client.AuthToken)
+	req.HttpReq.Header.Add("Content-Type", "application/json")
+	req.HttpReq.Header.Add("Accept", "application/json")
+	// retain the request body across multiple attempts
+	var body []byte
+	if req.HttpReq.Body != nil {
+		body, _ = io.ReadAll(req.HttpReq.Body)
+	}
+
+	var res Res
+
+	for attempts := 0; ; attempts++ {
+		req.HttpReq.Body = io.NopCloser(bytes.NewBuffer(body))
+		if req.LogPayload {
+			log.Printf("[DEBUG] HTTP Request: %s, %s, %s", req.HttpReq.Method, req.HttpReq.URL, req.HttpReq.Body)
+		} else {
+			log.Printf("[DEBUG] HTTP Request: %s, %s", req.HttpReq.Method, req.HttpReq.URL)
+		}
+
+		client.RateLimiterBucket.Wait(1) // Block until rate limit token available
+		httpRes, err := client.HttpClient.Do(req.HttpReq)
+		if err != nil {
+			if ok := client.Backoff(attempts); !ok {
+				log.Printf("[ERROR] HTTP Connection error occured: %+v", err)
+				log.Printf("[DEBUG] Exit from Do method")
+				return Res{}, err
+			} else {
+				log.Printf("[ERROR] HTTP Connection failed: %s, retries: %v", err, attempts)
+				continue
+			}
+		}
+
+		defer httpRes.Body.Close()
+		bodyBytes, err := io.ReadAll(httpRes.Body)
+		if err != nil {
+			if ok := client.Backoff(attempts); !ok {
+				log.Printf("[ERROR] Cannot decode response body: %+v", err)
+				log.Printf("[DEBUG] Exit from Do method")
+				return Res{}, err
+			} else {
+				log.Printf("[ERROR] Cannot decode response body: %s, retries: %v", err, attempts)
+				continue
+			}
+		}
+		res = Res(gjson.ParseBytes(bodyBytes))
+		if req.LogPayload {
+			log.Printf("[DEBUG] HTTP Response: %s", res.Raw)
+		}
+
+		if httpRes.StatusCode >= 200 && httpRes.StatusCode <= 299 {
+			log.Printf("[DEBUG] Exit from Do method")
+			break
+		} else {
+			if ok := client.Backoff(attempts); !ok {
+				log.Printf("[ERROR] HTTP Request failed: StatusCode %v", httpRes.StatusCode)
+				log.Printf("[DEBUG] Exit from Do method")
+				return res, fmt.Errorf("HTTP Request failed: StatusCode %v", httpRes.StatusCode)
+			} else if httpRes.StatusCode == 429 || (httpRes.StatusCode >= 500 && httpRes.StatusCode <= 599) {
+				log.Printf("[ERROR] HTTP Request failed: StatusCode %v, Retries: %v", httpRes.StatusCode, attempts)
+				continue
+			} else {
+				log.Printf("[ERROR] HTTP Request failed: StatusCode %v", httpRes.StatusCode)
+				log.Printf("[DEBUG] Exit from Do method")
+				return res, fmt.Errorf("HTTP Request failed: StatusCode %v", httpRes.StatusCode)
+			}
+		}
+	}
+
+	if res.Get("error.messages.0").Exists() {
+		log.Printf("[ERROR] JSON error: %s", res.Get("error.messages.0").String())
+		return res, fmt.Errorf("JSON error: %s", res.Get("error.messages.0").String())
+	}
+	return res, nil
+}
+
+// Get makes a GET request and returns a GJSON result.
+// Results will be the raw data structure as returned by FMC
+func (client *Client) Get(path string, mods ...func(*Req)) (Res, error) {
+	err := client.Authenticate()
+	if err != nil {
+		return Res{}, err
+	}
+	req := client.NewReq("GET", path, nil, mods...)
+	return client.Do(req)
+}
+
+// Delete makes a DELETE request.
+func (client *Client) Delete(path string, mods ...func(*Req)) (Res, error) {
+	err := client.Authenticate()
+	if err != nil {
+		return Res{}, err
+	}
+	req := client.NewReq("DELETE", path, nil, mods...)
+	return client.Do(req)
+}
+
+// Post makes a POST request and returns a GJSON result.
+// Hint: Use the Body struct to easily create POST body data.
+func (client *Client) Post(path, data string, mods ...func(*Req)) (Res, error) {
+	err := client.Authenticate()
+	if err != nil {
+		return Res{}, err
+	}
+	req := client.NewReq("POST", path, strings.NewReader(data), mods...)
+	return client.Do(req)
+}
+
+// Put makes a PUT request and returns a GJSON result.
+// Hint: Use the Body struct to easily create PUT body data.
+func (client *Client) Put(path, data string, mods ...func(*Req)) (Res, error) {
+	err := client.Authenticate()
+	if err != nil {
+		return Res{}, err
+	}
+	req := client.NewReq("PUT", path, strings.NewReader(data), mods...)
+	return client.Do(req)
+}
+
+// Login authenticates to the FMC instance.
+func (client *Client) Login() error {
+	for attempts := 0; ; attempts++ {
+		req := client.NewReq("POST", "/api/fmc_platform/v1/auth/generatetoken", strings.NewReader(""), NoLogPayload)
+		req.HttpReq.SetBasicAuth(client.Usr, client.Pwd)
+		httpRes, err := client.HttpClient.Do(req.HttpReq)
+		if err != nil {
+			return err
+		}
+		if httpRes.StatusCode != 204 {
+			log.Printf("[ERROR] Authentication failed: StatusCode %v", httpRes.StatusCode)
+			return fmt.Errorf("authentication failed, status code: %v", httpRes.StatusCode)
+		}
+		defer httpRes.Body.Close()
+		bodyBytes, _ := io.ReadAll(httpRes.Body)
+		if len(bodyBytes) > 0 {
+			if ok := client.Backoff(attempts); !ok {
+				log.Printf("[ERROR] Authentication failed: Invalid credentials")
+				return fmt.Errorf("authentication failed, invalid credentials")
+			} else {
+				log.Printf("[ERROR] Authentication failed: %s, retries: %v", err, attempts)
+				continue
+			}
+		}
+		client.AuthToken = httpRes.Header.Get("X-auth-access-token")
+		client.RefreshToken = httpRes.Header.Get("X-auth-refresh-token")
+		client.LastRefresh = time.Now()
+		client.RefreshCount = 0
+		client.DomainUUID = httpRes.Header.Get("DOMAIN_UUID")
+		log.Printf("[DEBUG] Authentication successful")
+		return nil
+	}
+}
+
+// Refresh refreshes the authentication token.
+// Note that this will be handled automatically by default.
+// Refresh will be checked every request and the token will be refreshed after 25 minutes.
+func (client *Client) Refresh() error {
+	for attempts := 0; ; attempts++ {
+		req := client.NewReq("POST", "/api/fmc_platform/v1/auth/refreshtoken", strings.NewReader(""), NoLogPayload)
+		req.HttpReq.Header.Add("X-auth-access-token", client.AuthToken)
+		req.HttpReq.Header.Add("X-auth-refresh-token", client.RefreshToken)
+		httpRes, err := client.HttpClient.Do(req.HttpReq)
+		if err != nil {
+			return err
+		}
+		if httpRes.StatusCode != 204 {
+			log.Printf("[ERROR] Authentication failed: StatusCode %v", httpRes.StatusCode)
+			return fmt.Errorf("authentication failed, status code: %v", httpRes.StatusCode)
+		}
+		defer httpRes.Body.Close()
+		bodyBytes, _ := io.ReadAll(httpRes.Body)
+		if len(bodyBytes) > 0 {
+			if ok := client.Backoff(attempts); !ok {
+				log.Printf("[ERROR] Authentication failed: Invalid credentials")
+				return fmt.Errorf("authentication failed, invalid credentials")
+			} else {
+				log.Printf("[ERROR] Authentication failed: %s, retries: %v", err, attempts)
+				continue
+			}
+		}
+		client.AuthToken = httpRes.Header.Get("X-auth-access-token")
+		client.RefreshToken = httpRes.Header.Get("X-auth-refresh-token")
+		client.LastRefresh = time.Now()
+		client.RefreshCount = client.RefreshCount + 1
+		client.DomainUUID = httpRes.Header.Get("DOMAIN_UUID")
+		log.Printf("[DEBUG] Refresh successful")
+		return nil
+	}
+}
+
+// Login if no token available.
+func (client *Client) Authenticate() error {
+	var err error
+	client.AuthenticationMutex.Lock()
+	if client.AuthToken != "" && time.Since(client.LastRefresh) > 1500*time.Second && client.RefreshCount < 3 {
+		err = client.Refresh()
+	} else if client.AuthToken == "" || (time.Since(client.LastRefresh) >= 1500*time.Second && client.RefreshCount >= 3) {
+		err = client.Login()
+	}
+	client.AuthenticationMutex.Unlock()
+	return err
+}
+
+// Backoff waits following an exponential backoff algorithm
+func (client *Client) Backoff(attempts int) bool {
+	log.Printf("[DEBUG] Begining backoff method: attempts %v on %v", attempts, client.MaxRetries)
+	if attempts >= client.MaxRetries {
+		log.Printf("[DEBUG] Exit from backoff method with return value false")
+		return false
+	}
+
+	minDelay := time.Duration(client.BackoffMinDelay) * time.Second
+	maxDelay := time.Duration(client.BackoffMaxDelay) * time.Second
+
+	min := float64(minDelay)
+	backoff := min * math.Pow(client.BackoffDelayFactor, float64(attempts))
+	if backoff > float64(maxDelay) {
+		backoff = float64(maxDelay)
+	}
+	backoff = (rand.Float64()/2+0.5)*(backoff-min) + min
+	backoffDuration := time.Duration(backoff)
+	log.Printf("[TRACE] Starting sleeping for %v", backoffDuration.Round(time.Second))
+	time.Sleep(backoffDuration)
+	log.Printf("[DEBUG] Exit from backoff method with return value true")
+	return true
+}
